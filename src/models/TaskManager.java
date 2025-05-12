a8fc70793c4a2f8a655dcd17b46ea61f9528257e
Oleg <gomzan.o@gmail.com>
2025-05-12 21:34:17 +0600

    added task selection and change of its status

--- src/models/TaskManager.java
+++ src/models/TaskManager.java
@@ -1,5 +1,7 @@
 package models;
 
+import jdk.jshell.Snippet;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,54 +16,91 @@ public class TaskManager {
     static Scanner sc = new Scanner(System.in);
     public static void toDoAppRun(){
         List<Task> tasks = new ArrayList<>();
-        addApp(tasks);
+        menuUser(tasks);
     }
 
-    private static void menuUser(){
+    private static void menuUser(List<Task> task){
+        while (true){
+            System.out.println("Выберите команду");
+            System.out.println("1 - Посмотреть все задачи");
+            System.out.println("2 - Добавить задачу");
+            System.out.println("3 - Выбрать задачу");
+            System.out.println("0 - Выход");
+            int chooseCommand = sc.nextInt();
+            switch (chooseCommand) {
+                case 1:
+                    printTask(task);
+                    break;
+                case 2:
+                    addApp(task);
+                    break;
+                case 3:
+                    choseApp(task);
+                    break;
+                case 0:
+                    return;
+                default:
+                    System.out.println("Нет такой команды");
+            }
 
+        }
     }
+
+
+    private static void printTask(List<Task> task) {
+        int i = 1;
+        System.out.println("----+--------+-------+--------+----------+-------+---------------+");
+        for (Task t : task) {
+            System.out.printf("|%d |%n|Название: %3s|%n|Описание: %3s|%n|Дата Создания: %3s||Дата Завершения: %3s|%n|Приоритет: %3s|Статус: %4s |%n",
+                    i++, t.getTitle(), t.getDescription(),  t.getCreatedDate(), t.getCompletionDate(),
+                     t.getPriority(), t.getState());
+        }
+        System.out.println("----+--------+-------+--------+----------+-------+---------------+");
+    }
+
+    private static void choseApp(List<Task> task){
+        printTask(task);
+        int indexApp = task.size();
+        while (indexApp >= task.size() || indexApp < 1){
+            System.out.printf("Введите номер задачи (1-%s)%n", task.size());
+            try{
+                indexApp = sc.nextInt();
+                sc.nextLine();
+                if(indexApp > task.size() || indexApp < 1){
+                    System.out.println("Данный не подходит давай по новой");
+                }else {
+                    Task t = task.get(indexApp-1);
+                    System.out.printf("|Название: %3s|%n|Описание: %3s|%n|Дата Создания: %3s||Дата Завершения: %3s|%n|Приоритет: %3s|Статус: %4s |%n",
+                             t.getTitle(), t.getDescription(),  t.getCreatedDate(), t.getCompletionDate(),
+                            t.getPriority(), t.getState());
+                    Status newState = addStatus();
+                    t.changeStatus(newState.name());
+                    break;
+                };
+            } catch (Exception e) {
+                System.out.println("Введите число");
+                sc.nextLine();
+            }
+        }
+    }
+
 
     private static void addApp(List<Task> task){
-        String addTitle = addTitle();
-        String addDescription = addDescription();
-        LocalDate addDate= addCompletionDate();
-        Priority newPriority = addPriority();
-        task.add(new Task(addTitle, addDescription, addDate ,newPriority));
+        String appTitle = addTitle();
+        String appDescription = addDescription();
+        LocalDate appDate= addCompletionDate();
+        Priority appPriority = addPriority();
+        task.add(new Task(appTitle, appDescription, appDate ,appPriority));
         System.out.println(task);
     }
 
-    private static Priority addPriority() {
-        System.out.println("Выберите приоритет");
-        System.out.println("1 - Низкий");
-        System.out.println("2 - Средний");
-        System.out.println("3 - Высокий");
-        int choosePriority = sc.nextInt();
-        Priority priority = null;
-        switch (choosePriority) {
-            case 1:
-                System.out.printf("Вы выбрали %s%n", Priority.LOW);
-                priority=  Priority.LOW;
-                break;
-            case 2:
-                System.out.printf("Вы выбрали %s%n", Priority.MEDIUM);
-                priority = Priority.MEDIUM;
-                break;
-            case 3:
-                System.out.printf("Вы выбрали %s%n", Priority.HIGH);
-                priority = Priority.HIGH;
-                break;
-            default:
-                System.out.println("Нет такой команды");
-        }
-        return priority;
-    }
 
     private static String addTitle(){
         String newTitle = "";
-        while (newTitle.isBlank()){
+        while (newTitle.isEmpty()){
             System.out.println("Введите название задачи");
             newTitle = sc.nextLine();
-            if(newTitle.isBlank()){
+            if(newTitle.isEmpty()){
                 System.out.println("Данный не подходит давай по проверяющий");
             }
         }
@@ -83,22 +122,70 @@ public class TaskManager {
     private static LocalDate addCompletionDate() {
         DateTimeFormatter dateCompletionDate = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String newDate = "";
-        LocalDate completion = null;
-        while (newDate.isBlank()) {
+        while (true) {
             System.out.println("Введите дату в формате dd.MM.yyyy");
             newDate = sc.nextLine();
-
             if (newDate.isBlank()) {
                 System.out.println("Данный не подходит давай по проверяющий");
             } else {
                 try {
-                    completion = LocalDate.parse(newDate, dateCompletionDate);
+                    return LocalDate.parse(newDate, dateCompletionDate);
                 } catch (DateTimeException e) {
                     System.out.println("Неверный формат даты: 22.07.2023");
                 }
             }
-
         }
-        return completion;
+    }
+
+    private static Priority addPriority() {
+        System.out.println("Выберите приоритет");
+        System.out.println("1 - Низкий");
+        System.out.println("2 - Средний");
+        System.out.println("3 - Высокий");
+        int choosePriority = sc.nextInt();
+        Priority priority = null;
+        switch (choosePriority) {
+            case 1:
+                System.out.printf("Вы выбрали %s приоритет%n", Priority.LOW);
+                priority=  Priority.LOW;
+                break;
+            case 2:
+                System.out.printf("Вы выбрали %s приоритет%n", Priority.MEDIUM);
+                priority = Priority.MEDIUM;
+                break;
+            case 3:
+                System.out.printf("Вы выбрали %s приоритет%n", Priority.HIGH);
+                priority = Priority.HIGH;
+                break;
+            default:
+                System.out.println("Нет такого приоритета");
+        }
+        return priority;
+    }
+
+    private static Status addStatus() {
+        System.out.println("Выберите статус");
+        System.out.println("1 - Новая");
+        System.out.println("2 - В работе");
+        System.out.println("3 - Выполнена");
+        int chooseStatus = sc.nextInt();
+        Status state = null;
+        switch (chooseStatus) {
+            case 1:
+                System.out.printf("У вас %s задача %n", Status.NEW);
+                state =  Status.NEW;
+                break;
+            case 2:
+                System.out.printf("Ваша задача в %s%n", Status.IN_PROGRESS);
+                state = Status.IN_PROGRESS;
+                break;
+            case 3:
+                System.out.printf("Вы %s задачу%n",  Status.DONE);
+                state = Status.DONE;
+                break;
+            default:
+                System.out.println("Нет задачи");
+        }
+        return state;
     }
 }
